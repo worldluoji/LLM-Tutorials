31f9da04ccbf761da7a6d475f6b9b570b787e945
worldluoji <qq1132416@outlook.com>
2025-04-09 17:32:10 +0800

    feat: demo2 to new version

--- catalyst/LangChain/langchain-demos/basic/langchain-demo2.py
+++ catalyst/LangChain/langchain-demos/basic/langchain-demo2.py
@@ -1,12 +1,19 @@
 
-import openai, os
-from langchain.prompts import PromptTemplate
-from langchain.llms import OpenAI
-from langchain.chains import LLMChain, SequentialChain
+import os
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
+from langchain_openai.chat_models.base import BaseChatOpenAI
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+from dotenv import load_dotenv
 
-llm = OpenAI(model_name="gpt-3.5-turbo-instruct", max_tokens=2048, temperature=0.5)
+load_dotenv()
+
+llm = BaseChatOpenAI(
+    model='deepseek-chat',  # 使用DeepSeek聊天模型
+    openai_api_key=os.environ.get("deepseek"),  # 替换为你的API易API密钥
+    openai_api_base='https://api.deepseek.com',  # API易的端点
+    max_tokens=1024  # 设置最大生成token数
+)
 
 q1_prompt = PromptTemplate(
     input_variables=["year1"],
@@ -21,11 +28,38 @@ q3_prompt = PromptTemplate(
     template="{team1}和{team2}哪只球队获得欧冠的次数多一些？"
 )
 
-# 可以连续问多个问题，然后把这些问题的答案，作为后续问题的输入来继续处理
-chain1 = LLMChain(llm=llm, prompt=q1_prompt, output_key="team1")
-chain2 = LLMChain(llm=llm, prompt=q2_prompt, output_key="team2")
-chain3 = LLMChain(llm=llm, prompt=q3_prompt)
+chain1 = (
+    RunnableParallel({"year1": RunnablePassthrough()}) # 接收  {"year1": 2000, "year2": 2010}中的 year1 作为输入
+    | q1_prompt
+    | llm
+    | (lambda x: {"team1": x.content})
+)
 
-sequential_chain = SequentialChain(chains=[chain1, chain2, chain3], input_variables=["year1", "year2"], verbose=True)
-answer = sequential_chain.run(year1=2000, year2=2010)
-print(answer)
+chain2 = (
+    RunnableParallel({"year2": RunnablePassthrough()})
+    | q2_prompt
+    | llm
+    | (lambda x: {"team2": x.content})
+)
+
+chain3 = (
+    # 同时接收 chain1 和 chain2 的输出（team1 和 team2）
+    RunnableParallel(team1=RunnablePassthrough(), team2=RunnablePassthrough())
+    | q3_prompt
+    | llm
+    | (lambda x: {"result": x.content})
+)
+
+sequential_chain = (
+    # 并行执行 chain1 和 chain2
+    RunnableParallel(
+        chain1=RunnablePassthrough() | chain1,  # RunnablePassthrough() | chain1 会先将 ​整个输入字典 传递给 chain1，即 {"year1": 2000, "year2": 2010}
+        chain2=RunnablePassthrough() | chain2   # 传递 year2 到 chain2
+    )
+    # 合并 chain1 和 chain2 的输出，传递给 chain3
+    | (lambda x: {"team1": x["chain1"]["team1"], "team2": x["chain2"]["team2"]})
+    | chain3
+)
+
+answer = sequential_chain.invoke({ "year1":2000, "year2":2010 })
+print(answer["result"])
