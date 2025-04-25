841d9c164601d815aa4d6f9e41e77181de4835d2
worldluoji <qq1132416@outlook.com>
2025-04-25 11:29:38 +0800

    feat: add code generate demo

---
+++ application/coding-demo/llm.py
@@ -0,0 +1,14 @@
+import os
+
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env")
+load_dotenv(dotenv_path=".env.local", override=True)
+
+def DeepSeek():
+    return ChatOpenAI(
+        model= "deepseek-v3",
+        api_key= os.environ.get("DASHSCOPE_API_KEY"),
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
