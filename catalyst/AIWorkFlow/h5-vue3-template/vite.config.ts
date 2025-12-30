65061c96f221430b39454bc71d3e8f1bb2143b3d
worldluoji <qq1132416@outlook.com>
2025-12-30 16:29:54 +0800

    feat: add autoprefixer and postcss-nested

--- catalyst/AIWorkFlow/h5-vue3-template/vite.config.ts
+++ catalyst/AIWorkFlow/h5-vue3-template/vite.config.ts
@@ -1,7 +1,9 @@
 import vue from '@vitejs/plugin-vue'
-import { VantResolver } from 'unplugin-vue-components/resolvers';
-import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
+import autoprefixer from 'autoprefixer'
+import postcssNested from 'postcss-nested'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +13,16 @@ export default defineConfig({
       resolvers: [VantResolver()],
     }),
   ],
+  css: {
+    // 进行 PostCSS 配置
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
+        }),
+        postcssNested()
+      ]
+    }
+  }
 })
