a5bb03603f2da7b0a42bb77f7c3d3261622d1907
worldluoji <qq1132416@outlook.com>
2026-07-30 20:39:18 +0800

    fix: add permission

--- catalyst/DevelopFramework/ClaudeAgentSDK/demo/claudesdkwithskill.py
+++ catalyst/DevelopFramework/ClaudeAgentSDK/demo/claudesdkwithskill.py
@@ -1,7 +1,7 @@
 import anyio
 import os
 
-from claude_agent_sdk import ClaudeSDKClient
+from claude_agent_sdk import ClaudeSDKClient,ClaudeAgentOptions
 
 
 os.environ.setdefault("ANTHROPIC_BASE_URL", "https://api.minimaxi.com/anthropic")
@@ -10,9 +10,14 @@ os.environ.setdefault("ANTHROPIC_SMALL_FAST_MODEL", "MiniMax-M3")
 os.environ.setdefault("ANTHROPIC_API_KEY", os.getenv("OPEN_AI_API_KEY"))
 
 
+options = ClaudeAgentOptions(
+    skills="all",
+    allowed_tools=["Read", "Write", "Bash", "Glob","Python"]
+)
+
 async def main():
-    async with ClaudeSDKClient() as client:
-        await client.query("获取一下拓维信息 002261 的资产负债表，保存为csv文件")
+    async with ClaudeSDKClient(options=options) as client:
+        await client.query("获取一下拓维信息 SZ002261 的资产负债表，保存为csv文件")
 
         # Extract and print response
         async for msg in client.receive_response():
